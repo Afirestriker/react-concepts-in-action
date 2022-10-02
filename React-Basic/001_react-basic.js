b9c6580bed38a34e8421d91515c466310a2dea16
Afirestriker <agrawalashish.in@gmail.com>
2022-10-02 21:36:47 +0530

    Develop First React Application

---
+++ React-Basic/001_react-basic.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <div>Hello World!</div> 
+            <div>Hello React!</div> 
+        </div>
+    );
+}
+ReactDOM.render(<App />, document.querySelector('#app'));
