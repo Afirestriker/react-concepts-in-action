cfcc2e9d0c9ba2b12d8d8ffb145a0aa2cfb1b478
Afirestriker <agrawalashish.in@gmail.com>
2023-01-29 18:36:58 +0530

    Added comment

--- React-Basic/001_react-basic.js
+++ React-Basic/001_react-basic.js
@@ -1,12 +1,15 @@
+/* App is user defined react component, which return some html content */
 function App() {
     return (
+        /* Writting HTML inside JavaScript */
         <div>
             <br />
             <h3>React example #1</h3>
-            <div>Hello World!</div> 
-            <div>Hello React!</div> 
+            <div>Hello World!</div>
+            <div>Hello React!</div>
         </div>
     );
 }
 
+/* Using ReactDOM library, we render react component into web page in HTML format */
 ReactDOM.render(<App />, document.querySelector('#app'));
