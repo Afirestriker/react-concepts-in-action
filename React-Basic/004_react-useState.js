082631616db508f953fa182b669025355e6d8101
Afirestriker <agrawalashish.in@gmail.com>
2023-02-04 09:22:04 +0530

    bug fixes on 004_react-useState

--- React-Basic/004_react-useState.js
+++ React-Basic/004_react-useState.js
@@ -1,10 +1,6 @@
 /* Destructing useState from react, to be able to directly use useSate() instead of React.useState() each time. */
 const { useState } = React;
 
-function updateCounter() {
-    setCounter(counter+1);
-}
-
 function AppUseState() {
     const [text, etText] = useState('Hello useState');
     const [counter, setCounter] = useState(1);
@@ -12,7 +8,7 @@ function AppUseState() {
         <div>
             <h3>React example #4 {text}</h3>
             <div>Counter value: {counter}</div>
-            {/* <button onClick={updateCounter()}>UpdateCoutner</button> */}
+            <button onClick={() => setCounter(counter+1)}>Update Counter</button>
         </div>
     );
 }
