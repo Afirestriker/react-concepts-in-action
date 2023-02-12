d1b640fda9ccd225376a67efd7f94fe3cc88a43a
Afirestriker <agrawalashish.in@gmail.com>
2023-02-12 16:59:30 +0530

    Updated  mathCalculations program

--- React-Apps/MathCalculation/mathCalculation.js
+++ React-Apps/MathCalculation/mathCalculation.js
@@ -1,39 +1,86 @@
-const operations = ['+', '-', '*', '/'];
-const num1 = Math.ceil(Math.random() * 10);  /*Generate random one digit number between 1-9*/
-const num2 = Math.ceil(Math.random() * 100); /*Generate random two digit number between 10-99*/
-const operationType = Math.floor(Math.random()*operations.length);
-
-const EquationItem = (props) => {
-    return (
-        <div id={props.id} className="equationBox">{props.item}</div>
-    );
-}
-
-const Equation = () => {
-    return (
-        <div className="equation">
-            <EquationItem id="num1" item={num1} />
-            <EquationItem id="opType" item={operations[operationType]} />
-            <EquationItem id="num2" item={num2} />
-            <EquationItem item="=" />
-            <input type="text" id="result" name="result" placeholder="Result" />
-        </div>
-    );
-}
-
-function checkResult() {
-    debugger;
-    let resultVal = document.querySelector('#result');
-}
+const {useState} = React;
 
 const App_MathCalculation = () => {
+    
+    const [state, setState] = useState({
+        num1: Math.ceil(Math.random() * 10),  /*Generate random one digit number between 1-10*/
+        num2: Math.ceil(Math.random() * 100), /*Generate random number between 1-99*/
+        MathOperands: ['+', '-', '*', '/'],
+        operandsLength: 4,                    /*Length of MathOperands array*/
+        opeationType: "+",
+        response: "",
+        score: 0        
+    });
+    
+    function updateResponse(event) {
+        setState({
+            ...state,
+            response: event.target.value,
+        });
+    }
+
+    function inputKeyPress(event) {
+        if(event.key === "Enter") {
+            let equationResult = 0;
+            
+            if(state.opeationType == "+")   equationResult = state.num1 + state.num2;
+            if(state.opeationType == "-")   equationResult = state.num1 - state.num2;
+            if(state.opeationType == "*")   equationResult = state.num1 * state.num2;
+            if(state.opeationType == "/")   equationResult = parseInt(state.num1 / state.num2);
+
+            if(equationResult == state.response) {
+                setState({
+                    ...state,
+                    num1: Math.ceil(Math.random() * 100),
+                    num2: Math.ceil(Math.random() * 10),
+                    opeationType: state.MathOperands[Math.floor(Math.random()*state.operandsLength)],
+                    score: state.score + 1
+                });
+            } else {
+                setState({
+                    ...state,
+                    score: state.score - 1
+                });
+            }
+        }
+    }
+
+    const EquationItem = (props) => {
+        return (
+            <div className="equationBox">{props.item}</div>
+        );
+    }
+
+    const Equation = () => {
+        return (
+            <div>
+                <div className="equationItems">
+                    <EquationItem item={state.num1} />
+                    <EquationItem item={state.opeationType} />
+                    <EquationItem id="num2" item={state.num2} />
+                </div>
+                <input autoFocus={true} type="text" value={state.response} placeholder="Answer" onChange={updateResponse} onKeyPress={inputKeyPress} />
+                <div>Score: {state.score}</div>
+            </div>
+        );
+    }
+
+    if(state.score == 10) {
+        return (
+            <div className="equation won">
+                YOU WON!!
+            </div>
+        );
+    }
+
     return (
         <div>
-            <h3>Let have Some Fun Mathematical Calculations</h3>
+            <h3>Let have Some Tricky Mathematical Calculations</h3>
             <hr/>
-            <div><i>Evaluate the following equation:</i></div> 
-            <Equation />
-            <div><button onClick={checkResult()}>Check</button></div>
+            <i>Evaluate the following equation:</i>
+            <div className="equation">
+                <Equation />
+            </div>
         </div>
     );
 }
