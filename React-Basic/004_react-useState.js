63b83de785ac94aeffe3ece3b2b1d7fbf6125916
Afirestriker <agrawalashish.in@gmail.com>
2023-02-12 11:28:45 +0530

    Updated react-useState to update counter using function

--- React-Basic/004_react-useState.js
+++ React-Basic/004_react-useState.js
@@ -1,14 +1,18 @@
-/* Destructing useState from react, to be able to directly use useSate() instead of React.useState() each time. */
+/* Destructing useState from react, to directly use useSate() instead of React.useState() each time. */
 const { useState } = React;
 
 function AppUseState() {
-    const [text, etText] = useState('Hello useState');
-    const [counter, setCounter] = useState(1);
+    const [text, etText] = useState('useState');
+    const [counter, setCounter] = useState(0);
+
+    /*Updating counter value usin setCounter()*/
+    const updateCounter = () => setCounter(counter+1);
+
     return (
         <div>
-            <h3>React example #4 {text}</h3>
-            <div>Counter value: {counter}</div>
-            <button onClick={() => setCounter(counter+1)}>Update Counter</button>
+            <h4>React example #4 {text}</h4>
+            <h1>Counter: {counter}</h1>
+            <button onClick={updateCounter}>Update</button>
         </div>
     );
 }
