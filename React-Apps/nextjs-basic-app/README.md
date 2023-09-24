86a8128c40cbd242e5324762abdaa6020a1beb78
Afirestriker <agrawalashish.in@gmail.com>
2023-09-24 11:31:03 +0530

    Built nextjs-basic-app

---
+++ React-Apps/nextjs-basic-app/README.md
@@ -0,0 +1,15 @@
+## Project Run Steps
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) on your browser to see the result.
---
+++ React-Apps/nextjs-basic-app/src/app/page.js
@@ -0,0 +1,70 @@
+"use client"
+
+import { useState } from "react";
+
+function Title(props) {
+    return (
+        <div>
+            <h1> {props.title} </h1>
+            {/* using curly braces we can write javascript inside jsx */}
+        </div>
+    );
+}
+
+function Header({ context }) {
+    return (
+        <>
+            <h2> {context} </h2>
+        </>
+    );
+}
+
+// Iterating through lists
+function Names({ nameList = [] }) {
+    return (
+        <ul>
+            {nameList.map((name) => {
+                return (
+                    <li id={name} key={name}>
+                        {name}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+
+// Interactivity with State and event handlers in react
+function ToggleButton() {
+    const [visibility, setVisibility] = useState("initial");
+
+    function clickHandlerFunction() {
+        setVisibility(visibility === "initial" ? "none" : "initial");
+    }
+
+    return (
+        <>
+            <button onClick={clickHandlerFunction}>
+                {" "}
+                {visibility == "none" ? "Show" : "Hide"} Text &gt;{" "}
+            </button>
+            <span style={{ display: visibility }}>
+                {" "}
+                React Event Handling Using State{" "}
+            </span>
+        </>
+    );
+}
+
+export default function Homepage() {
+    return (
+        <>
+            <Title title="React Practise" />
+            <Header context="Run...Build...Deploy...🚀" />
+            <Names
+                nameList={["Kate", "Mike", "Jake", "Harry", "Hermione", "Ron"]}
+            />
+            <ToggleButton />
+        </>
+    );
+}
