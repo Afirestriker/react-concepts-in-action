442ca1941cad7adf9e544db04bb50277e81a4f97
Ashish Agrawal <agrawalashish.in@gmail.com>
2023-10-25 09:42:17 +0530

    Created Greet component

---
+++ ReactJS-Tutorial/hello-world/src/components/Greet.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// function Greet(props) {
+//     return(<h2> Welcome {props.name} </h2>)
+// }
+
+
+// const Greet = props => {
+//     return(
+//         <h2> Welcome {props.name} </h2>
+//     );
+// }
+
+
+const Greet = props => <h2>Welcome {props.name}</h2>
+
+export default Greet;
