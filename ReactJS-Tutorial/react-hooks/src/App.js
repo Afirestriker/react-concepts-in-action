51971f2badb9619b1b1a1818971847cd5711f627
Afirestriker <agrawalashish.in@gmail.com>
2024-01-07 19:44:42 +0530

    Lesson 2 - useState Hook

--- ReactJS-Tutorial/react-hooks/src/App.js
+++ ReactJS-Tutorial/react-hooks/src/App.js
@@ -7,7 +7,7 @@ function App() {
     return (
         <div className="App">
             <HookCounter />
-            <ClassCounter />
+            {/* <ClassCounter /> */}
         </div>
     );
 }
