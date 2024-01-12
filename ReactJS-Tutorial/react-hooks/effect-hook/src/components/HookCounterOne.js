981bcbcf61fa304021827c339e3ac22c754f3441
Afirestriker <agrawalashish.in@gmail.com>
2024-01-12 08:59:51 +0530

    Implement useEffect in HookCounterOne.js

--- ReactJS-Tutorial/react-hooks/effect-hook/src/components/HookCounterOne.js
+++ ReactJS-Tutorial/react-hooks/effect-hook/src/components/HookCounterOne.js
@@ -3,6 +3,10 @@ import React, {useState, useEffect} from 'react';
 const HookCounterOne = () => {
     const [count, setCount] = useState(0);
 
+    useEffect(() => {
+        document.title = `Clicked ${count} times`;
+    });
+
     return(
         <div>
             <button onClick={() => setCount(prevState => prevState + 1)}>Count {count}</button>
