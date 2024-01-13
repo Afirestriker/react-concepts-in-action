db1d588d75045df4c95c2e25686352ebe7727248
Afirestriker <agrawalashish.in@gmail.com>
2024-01-13 15:38:56 +0530

    Update MouseContainer.js

--- ReactJS-Tutorial/react-hooks/effect-hook/src/App.js
+++ ReactJS-Tutorial/react-hooks/effect-hook/src/App.js
@@ -5,11 +5,13 @@ import HookCounterOne from "./components/HookCounterOne";
 import HookCounterTwo from "./components/HookCounterTwo";
 import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
+import MouseContainer from "./components/MouseContainer";
 
 function App() {
     return <div className="App">
         Effect-Hook
-        <HookMouse />
+        <MouseContainer />
+        {/* <HookMouse /> */}
         {/* <ClassMouse /> */}
         {/* <HookCounterTwo /> */}
         {/* <HookCounterOne /> */}
