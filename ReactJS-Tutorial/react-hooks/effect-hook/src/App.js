9d797531db86c06bf8f00210d5c291adba36e098
Afirestriker <agrawalashish.in@gmail.com>
2024-01-14 10:00:28 +0530

    Import and Run IntervalHookCounter component in App

--- ReactJS-Tutorial/react-hooks/effect-hook/src/App.js
+++ ReactJS-Tutorial/react-hooks/effect-hook/src/App.js
@@ -7,11 +7,13 @@ import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
 import MouseContainer from "./components/MouseContainer";
 import IntervalClassCounter from "./components/IntervalClassCounter";
+import IntervalHookCounter from "./components/IntervalHookCounter";
 
 function App() {
     return <div className="App">
         Effect-Hook
-        <IntervalClassCounter />
+        <IntervalHookCounter />
+        {/* <IntervalClassCounter /> */}
         {/* <MouseContainer /> */}
         {/* <HookMouse /> */}
         {/* <ClassMouse /> */}
