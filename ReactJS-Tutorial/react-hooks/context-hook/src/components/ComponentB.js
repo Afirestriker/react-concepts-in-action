5d19724ebe730f2cb89026f41140dcb14955acaf
Afirestriker <agrawalashish.in@gmail.com>
2024-01-23 08:13:59 +0530

    update ComponentB.js

---
+++ ReactJS-Tutorial/react-hooks/context-hook/src/components/ComponentB.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ComponentB extends Component {
+  render() {
+    return (
+      <div>ComponentB</div>
+    )
+  }
+}
+
+export default ComponentB
