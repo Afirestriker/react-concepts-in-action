0c2725422e9a36b7ffd3fab58d3b27279d314675
Afirestriker <agrawalashish.in@gmail.com>
2024-01-24 08:24:34 +0530

    Update ComponentD.js
    using useContext in React functional component

--- ReactJS-Tutorial/react-hooks/context-hook/src/components/ComponentD.js
+++ ReactJS-Tutorial/react-hooks/context-hook/src/components/ComponentD.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { userContext, userTwoContext } from "../App";
 
 function ComponentD() {
-    return <div>ComponentD</div>;
+    const userContextVal = useContext(userContext);
+    const userTwoContextVal = useContext(userTwoContext);
+
+    return (
+        <div>
+            <h4>useContext in React Functional Component</h4>
+            User Context: <h5>{userContextVal}</h5>
+            User Two Context: <h5>{userTwoContextVal}</h5>
+        </div>
+    );
 }
 
 export default ComponentD;
