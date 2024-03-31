e5d7cbd793d26ff2e6538b3832d341bd511debee
Afirestriker <agrawalashish.in@gmail.com>
2024-03-31 15:13:50 +0530

    Create component CounterOne.js

--- ReactJS-Tutorial/react-hooks/reducer-hook/src/App.js
+++ ReactJS-Tutorial/react-hooks/reducer-hook/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import CounterOne from "./components/CounterOne";
 
 function App() {
     return (
         <div className="App">
-
+            <CounterOne />
         </div>
     );
 }
