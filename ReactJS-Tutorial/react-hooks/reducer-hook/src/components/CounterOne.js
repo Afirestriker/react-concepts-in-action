792f8341f562d35974a1f3e367c8487d9b37050e
Afirestriker <agrawalashish.in@gmail.com>
2024-05-25 18:30:29 +0530

    Implement UseReducer hook (simple state and action)

--- ReactJS-Tutorial/react-hooks/reducer-hook/src/components/CounterOne.js
+++ ReactJS-Tutorial/react-hooks/reducer-hook/src/components/CounterOne.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+const initialValue = 0;
+
+const reducer = (state, action) => {
+    switch(action) {
+        case 'increment': return state + 1;
+        case 'decrement': return state - 1;
+        case 'reset': return initialValue;
+        default: return state;
+    }
+};
 
 const CounterOne = () => {
+    const [count, dispatch] = useReducer(reducer, initialValue);
+
+    console.log("CounterOne Rendered")
 
     return (
         <div>
-
+            <h1>Counter: {count}</h1>
+            <button onClick={() => dispatch('increment')}>Increment</button>
+            <button onClick={() => dispatch('decrement')}>Decrement</button>
+            <button onClick={() => dispatch('reset')}>Reset</button>
         </div>
-    )
+    );
 }
 
 export default CounterOne;
