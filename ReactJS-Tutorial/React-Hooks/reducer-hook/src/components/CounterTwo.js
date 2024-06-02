2e9ebdd4f85dbdb24a631667e52082410a92f8bd
Afirestriker <agrawalashish.in@gmail.com>
2024-06-02 11:34:47 +0530

    feat: Lesson 20 updates to demonstrates managing multiple states within a single useReducer hook.
    Added a new counter and corresponding actions to the reducer function, allowing independent control of two counters within the component. This enhances the component's functionality and demonstrates managing multiple states within a single reducer.

--- ReactJS-Tutorial/React-Hooks/reducer-hook/src/components/CounterTwo.js
+++ ReactJS-Tutorial/React-Hooks/reducer-hook/src/components/CounterTwo.js
@@ -3,15 +3,20 @@
 import React, { useReducer } from 'react';
 
 const initialState = {
-    firstCounter: 0
+    firstCounter: 0,
+    secondCounter: 10
 };
 
 const reducer = (state /* current state */, action /* action to perform */) => {
     switch(action.type) {
         case 'increment':
-            return { firstCounter: state.firstCounter + (action.value??1) };
+            return { ...state, firstCounter: state.firstCounter + (action.value??1) };
         case 'decrement':
-            return { firstCounter: state.firstCounter - (action.value??1) };
+            return { ...state, firstCounter: state.firstCounter - (action.value??1) };
+        case 'incrementCounter2':
+            return { ...state, secondCounter: state.secondCounter + (action.value??1) };
+        case 'decrementCounter2':
+            return { ...state, secondCounter: state.secondCounter - (action.value??1) };
         case 'reset':
             return initialState;
         case 'default':
@@ -24,11 +29,25 @@ const CounterTwo = (props) => {
 
     return (
         <div>
-            <div>{count.firstCounter}</div>
-            <button onClick={() => dispatch({type: 'increment', value: 1})}>Increment</button>
-            <button onClick={() => dispatch({type: 'decrement', value: 1})}>Decrement</button>
-            <button onClick={() => dispatch({type: 'increment', value: 5})}>Increment 5</button>
-            <button onClick={() => dispatch({type: 'decrement', value: 5})}>Decrement 5</button>
+            <div>First Counter: {count.firstCounter}</div>
+            <div>Second Counter: {count.secondCounter}</div>
+
+            <div>
+                {/* Counter One Action */}
+                <button onClick={() => dispatch({type: 'increment', value: 1})}>Increment</button>
+                <button onClick={() => dispatch({type: 'decrement', value: 1})}>Decrement</button>
+                <button onClick={() => dispatch({type: 'increment', value: 5})}>Increment 5</button>
+                <button onClick={() => dispatch({type: 'decrement', value: 5})}>Decrement 5</button>
+            </div>
+
+            <div>
+                {/* Counter Two Action */}
+                <button onClick={() => dispatch({type: 'incrementCounter2', value: 1})}>Increment Counter Two</button>
+                <button onClick={() => dispatch({type: 'decrementCounter2', value: 1})}>Decrement Counter Two</button>
+                <button onClick={() => dispatch({type: 'incrementCounter2', value: 5})}>Increment Counter Two 5</button>
+                <button onClick={() => dispatch({type: 'decrementCounter2', value: 5})}>Decrement Counter Two 5</button>
+            </div>
+
             <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
         </div>
     );
