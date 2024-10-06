24ef35676c5f069fe46fdc8d3efa735bcc559a1b
Afirestriker <agrawalashish.in@gmail.com>
2024-10-06 09:08:00 +0530

    refactor: improve code redability

--- react-code-splitting/src/App.jsx
+++ react-code-splitting/src/App.jsx
@@ -3,8 +3,8 @@ import react, { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
+const Home    = lazy(() => import("./pages/Home"));
+const About   = lazy(() => import("./pages/About"));
 const Profile = lazy(() => import("./pages/Profile"));
 const Contact = lazy(() => import("./pages/Contact"));
 
@@ -17,8 +17,8 @@ function App() {
                 {/* Common Suspense for all routes */}
                 <Suspense fallback={<div>Loading...</div>}>
                     <Routes>
-                        <Route path="/home" element={ <Home />} />
-                        <Route path="/about" element={ <About/>} />
+                        <Route path="/home"    element={ <Home />} />
+                        <Route path="/about"   element={ <About/>} />
                         <Route path="/profile" element={ <Profile />} />
                         <Route path="/contact" element={ <Contact />} />
 
@@ -27,14 +27,16 @@ function App() {
                 </Suspense>
 
                 {/* Separate Suspense under each route */}
-                {/* <Routes>
-                    <Route path="/home" element={ <Suspense fallback={<div>Loading Home...</div>}> <Home /> </Suspense> } />
-                    <Route path="/about" element={ <Suspense fallback={<div>Loading About...</div>}> <About/> </Suspense> } />
-                    <Route path="/profile" element={ <Suspense fallback={<div>Loading Profile...</div>}> <Profile /> </Suspense> } />
-                    <Route path="/contact" element={ <Suspense fallback={<div>Loading Contact...</div>}> <Contact /> </Suspense> } />
+                {/*
+                    <Routes>
+                        <Route path="/home"    element={ <Suspense fallback={<div>Loading Home...   </div>}> <Home />    </Suspense> } />
+                        <Route path="/about"   element={ <Suspense fallback={<div>Loading About...  </div>}> <About/>    </Suspense> } />
+                        <Route path="/profile" element={ <Suspense fallback={<div>Loading Profile...</div>}> <Profile /> </Suspense> } />
+                        <Route path="/contact" element={ <Suspense fallback={<div>Loading Contact...</div>}> <Contact /> </Suspense> } />
 
-                    <Route path="*" element={<Navigate to="/home" replace />} />
-                </Routes> */}
+                        <Route path="*" element={<Navigate to="/home" replace />} />
+                    </Routes>
+                */}
             </BrowserRouter>
         </>
     );
